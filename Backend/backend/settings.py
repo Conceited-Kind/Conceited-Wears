5f76e5ee098e7d2f37f7e403218a1c5a1d8ad730
Allan Kinuva <allankinuva@gmail.com>
2026-06-03 15:08:29 +0300

    Fixed CORS Configuration

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -78,7 +78,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ALL_ORIGINS = False
+CORS_ALLOWED_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://conceited-wears.netlify.app",
